9baa4a2def12f7ce8688c6a00f61fc61af3633f4
Marco Kuoni <marco@lemonbrain.ch>
2019-06-07 19:07:32 +0200

    Remove exception cause we have now package dependency

--- src/GraphQl/TestGraphQl.php
+++ src/GraphQl/TestGraphQl.php
@@ -3,18 +3,12 @@ namespace GraphQl;
 
 use GraphQl\PersonResolver;
 use Concrete5GraphqlWebsocket\GraphQl\SchemaBuilder;
-use Exception;
 
 class TestGraphQl
 {
     public static function start()
     {
-        //if somebody forgott to install the GraphQL Websocket Package in Concrete5
-        if (class_exists('Concrete5GraphqlWebsocket\GraphQl\SchemaBuilder')) {
-            SchemaBuilder::registerSchemaFileForMerge(__DIR__ . '/person.gql');
-            SchemaBuilder::registerResolverForMerge(PersonResolver::get());
-        } else {
-            throw new Exception(t('You must install the GraphQL Websocket Package in Concrete5. Navigate to your Composer root directory and use "./vendor/bin/concrete5 c5:package-install concrete5_graphql_websocket" or do it through the dashboard of Concrete5.'));
-        }
+        SchemaBuilder::registerSchemaFileForMerge(__DIR__ . '/person.gql');
+        SchemaBuilder::registerResolverForMerge(PersonResolver::get());
     }
 }
